a5fa54fa87fede57c49b293a035a59416bcb6299
guguloththirumalesh <146948828+guguloththirumalesh@users.noreply.github.com>
2023-10-04 17:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let menu = doucument.querySelecter("#menu-btn");
+let navbar = doucument.querySelecter(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
